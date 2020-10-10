8363a81f45c4f2dd65ae08c058ce8446a0094a78
teteus <matheusflorentino100@gmail.com>
2020-10-10 02:06:32 -0300

    Criado a classe categoria

---
+++ src/main/java/com/api/springboot/model/Produto.java
@@ -0,0 +1,86 @@
+package com.api.springboot.model;
+
+import java.time.LocalDateTime;
+
+public class Produto {
+    private Long id;
+    private String nome;
+    private float preco;
+    private float peso;
+    private boolean tipo;
+    private boolean ifPromocao;
+    private Promocao promocao;
+    private LocalDateTime criadoEm;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public boolean isTipo() {
+        return tipo;
+    }
+
+    public void setTipo(boolean tipo) {
+        this.tipo = tipo;
+    }
+
+    public boolean isIfPromocao() {
+        return ifPromocao;
+    }
+
+    public void setIfPromocao(boolean ifPromocao) {
+        this.ifPromocao = ifPromocao;
+    }
+
+    public Promocao getPromocao() {
+        return promocao;
+    }
+
+    public void setPromocao(Promocao promocao) {
+        this.promocao = promocao;
+    }
+
+    public LocalDateTime getCriadoEm() {
+        return criadoEm;
+    }
+
+    public Produto(String nome, float preco, float peso, boolean tipo,boolean ifPromocao, Promocao promocao) {
+        this.nome = nome;
+        this.preco = preco;
+        this.peso = peso;
+        this.tipo = tipo;
+        this.ifPromocao = ifPromocao;
+        this.promocao = promocao;
+        this.criadoEm = LocalDateTime.now();
+    }
+    public Produto(){this.criadoEm = LocalDateTime.now();}
+
+}

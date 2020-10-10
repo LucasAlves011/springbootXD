33ab7098f46b922765d23dd6efc3f9671e6daebb
PauloRRRRR <notadead5@gmail.com>
2020-10-10 01:20:48 -0300

    Criando a classe comprador

---
+++ src/main/java/com/api/springboot/model/Comprador.java
@@ -0,0 +1,59 @@
+package com.api.springboot.model;
+import java.time.*;
+
+public class Comprador {
+    private long id;
+    private Produto produto;
+    private int quantidade;
+    private float precoTotal;
+    private LocalDateTime criadoEm;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public float getPrecoTotal() {
+        return precoTotal;
+    }
+
+    public void setPrecoTotal(float precoTotal) {
+        this.precoTotal = precoTotal;
+    }
+
+    public LocalDateTime getCriadoEm() {
+        return criadoEm;
+    }
+
+    public Comprador(Produto produto, int quantidade, float precoTotal) {
+
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.precoTotal = precoTotal;
+        this.criadoEm = LocalDateTime.now();
+    }
+
+    public Comprador() {
+        this.criadoEm = LocalDateTime.now();
+    }
+
+}
